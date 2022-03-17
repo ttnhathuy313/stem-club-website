a2490f721316e135c809f5ce31875c95a81008a8
nhomnhomm <khuetutran14638@gmail.com>
2022-03-17 15:01:11 +0700

    commit changes to navbar & hero draft

--- website-frontend/src/components/Home.js
+++ website-frontend/src/components/Home.js
@@ -1,14 +1,31 @@
 // @nhomnhomm Complete this
-const Hero = () => {
+import cover from '../images/cover.png'
+import decoration from '../images/decoration.png'
+
+const Hero = ({vw, vh}) => {
     return (
-        <h1>This is Fulbright STEM Club.</h1>
+        <div className="container">
+            <div className="row vh-100 align-items-center">
+                <div className="col-5">
+                    <h1 className="text-secondary">Fulbright <br></br> STEM Club.</h1>
+                    <h2 className="text-primary">Leaders of the next <br></br> technological revolution</h2>
+                </div>
+                <div className="col-7 position-relative">
+                    <img src={decoration} alt="Decoration" width="20%" className="position-absolute top-50 z-index-1"></img>
+                    <img src={cover} alt="Cover" width="80%"></img>
+                </div>
+            </div>
+        </div>
     )
 }
 
 
 const Home = () => {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
     return (
-        <Hero/>
+        <Hero vw = {vw} vh = {vh}/>
     )
 }
 
--- website-frontend/src/components/NavBar.js
+++ website-frontend/src/components/NavBar.js
@@ -1,22 +1,34 @@
 // @nhomnhomm Complete this
+import {Link} from 'react-router-dom'
 import logo from '../images/logo.png'
 
 const NavLink = ({path, text}) => {
-  return <a className = 'nav-link' href = {path} target='_self'>{text}</a>
+  return (
+    <Link to = {path} className='nav-item nav-link pb-0 pt-0'>
+      <button className='btn text-primary'>{ text }</button>
+    </Link>
+  )
 }
 
 const NavBar = () => {
   return (
-    <div className = 'navbar text-primary border-bottom secondary'>
-      <a className = 'navbar-brand' href = '/' target ='_self'>
-        <img src={logo} width = '50' height = '50' alt ='Logo' className='d-inline-block align-top'></img>
-      </a>
-      <div className='ms-auto d-flex'>
-        <NavLink path = '/' text = 'Home'/>
-        <NavLink path = '/about' text = 'About Us'/>
-        <NavLink path = '/activities' text = 'Activities'/>
-        <NavLink path = '/blog' text = 'Blog'/>
-        <NavLink path = '/podcast' text = 'Podcast'/>
+    <div className = 'navbar navbar-expand-lg navbar-light border-bottom mb-4 pt-2 pb-2 p-4'>
+      <Link to='/' className = 'navbar-brand'>
+        <img src={logo} width = '40' height = '40' alt ='Logo'></img>
+      </Link>
+
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarToggler" aria-controls="navBarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon text-primary"></span>
+      </button>
+
+      <div className='collapse navbar-collapse' id ='navBarToggler'>
+        <div className = 'navbar-nav ms-auto'>
+          <NavLink path = '/' text = 'Home'/>
+          <NavLink path = '/about' text = 'About Us'/>
+          <NavLink path = '/activities' text = 'Activities'/>
+          <NavLink path = '/blog' text = 'Blog'/>
+          <NavLink path = '/podcast' text = 'Podcast'/>
+        </div>
       </div>
     </div>
   )

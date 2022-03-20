c7bcb0350d879ef6fd60a30ea037668132fa0420
nhomnhomm <khuetutran14638@gmail.com>
2022-03-21 01:32:10 +0700

    commit changes to hero section

--- website-frontend/src/components/Home.js
+++ website-frontend/src/components/Home.js
@@ -7,8 +7,8 @@ const Hero = () => {
         <div className="container" id="hero">
             <div className="row h-100 align-items-center">
                 <div className="col-5">
-                    <h1 className="text-secondary">Fulbright <br></br> STEM Club.</h1>
-                    <h2 className="text-primary">Leaders of the next <br></br> technological revolution</h2>
+                    <h1 className="text-secondary fw-bold">Fulbright <br></br> STEM Club</h1>
+                    <h2 className="text-primary fw-bold">Leaders of the next technological revolution</h2>
                 </div>
                 <div className="col-7">
                     <img src={cover} alt="Cover" width="80%" className="position-relative start-10"></img>

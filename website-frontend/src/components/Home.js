b71f982c26f94dee347d50d97dfa14baedb606c9
nhomnhomm <khuetutran14638@gmail.com>
2022-03-20 19:30:45 +0700

    commit hero

--- website-frontend/src/components/Home.js
+++ website-frontend/src/components/Home.js
@@ -2,17 +2,17 @@
 import cover from '../images/cover.png'
 import decoration from '../images/decoration.png'
 
-const Hero = ({vw, vh}) => {
+const Hero = () => {
     return (
-        <div className="container">
+        <div className="container" id="hero">
             <div className="row vh-100 align-items-center">
                 <div className="col-5">
                     <h1 className="text-secondary">Fulbright <br></br> STEM Club.</h1>
                     <h2 className="text-primary">Leaders of the next <br></br> technological revolution</h2>
                 </div>
-                <div className="col-7 position-relative">
-                    <img src={decoration} alt="Decoration" width="20%" className="position-absolute top-50 z-index-1"></img>
-                    <img src={cover} alt="Cover" width="80%"></img>
+                <div className="col-7">
+                    <img src={cover} alt="Cover" width="80%" className="position-relative start-10"></img>
+                    <img src={decoration} alt="Decoration" width="20%" className="position-relative start-n80"></img>
                 </div>
             </div>
         </div>
@@ -21,11 +21,8 @@ const Hero = ({vw, vh}) => {
 
 
 const Home = () => {
-    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-
     return (
-        <Hero vw = {vw} vh = {vh}/>
+        <Hero/>
     )
 }
 

c42dbaa02db803f2bd8f3b0e9a0a2a3c84e3dd18
npnkhoi <npnkhoi@gmail.com>
2022-03-20 21:38:53 +0700

    update hover style for nav buttons

--- website-frontend/src/components/NavBar.js
+++ website-frontend/src/components/NavBar.js
@@ -1,20 +1,21 @@
 // @nhomnhomm Complete this
 import {Link} from 'react-router-dom'
 import logo from '../images/logo.png'
+import './NavBar.scss'
 
 const NavLink = ({path, text}) => {
   return (
-    <Link to = {path} className='nav-item nav-link pb-0 pt-0'>
-      <button className='btn text-primary'>{ text }</button>
+    <Link to = {path} className='nav-item nav-link nav-btn text-primary px-3 py-3'>
+      { text }
     </Link>
   )
 }
 
 const NavBar = () => {
   return (
-    <div className = 'navbar navbar-expand-lg navbar-light border-bottom mb-4 pt-2 pb-2 p-4'>
-      <Link to='/' className = 'navbar-brand'>
-        <img src={logo} width = '40' height = '40' alt ='Logo'></img>
+    <div className = 'navbar navbar-expand-lg navbar-light border-bottom mb-4 py-0'>
+      <Link to='/' className = 'navbar-brand ms-4'>
+        <img src={logo} width = '40' height = '40' alt ='Logo' ></img>
       </Link>
 
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarToggler" aria-controls="navBarToggler" aria-expanded="false" aria-label="Toggle navigation">

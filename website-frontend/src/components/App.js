27d1ac203e0652fea257b5e69bb2a0d1db10e4ac
nhomnhomm <khuetutran14638@gmail.com>
2022-04-18 19:25:55 +0700

    commit updated home page

--- website-frontend/src/components/App.js
+++ website-frontend/src/components/App.js
@@ -11,7 +11,7 @@ import Podcast from './Podcast'
 const Main = () => {
   return (
 
-    <div className="container">
+    <div className="container-fluid px-0 m-0">
       <Routes className='main container'>
         <Route path='/' element={ <Home /> } />
         <Route path='/about' element={ <About /> } />
--- website-frontend/src/components/NavBar.js
+++ website-frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ import './NavBar.scss'
 
 const NavLink = ({path, text}) => {
   return (
-    <Link to = {path} className='nav-item nav-link nav-btn text-primary px-3 py-3'>
+    <Link to = {path} className='nav-item nav-link nav-btn fw-bold text-white px-3 py-3'>
       { text }
     </Link>
   )
@@ -13,18 +13,17 @@ const NavLink = ({path, text}) => {
 
 const NavBar = () => {
   return (
-    <div className = 'navbar navbar-expand-lg navbar-light border-bottom mb-4 py-0'>
+    <div className = 'navbar navbar-expand-lg navbar-dark bg-primary py-0 px-5 sticky-top'>
       <Link to='/' className = 'navbar-brand ms-4'>
         <img src={logo} width = '40' height = '40' alt ='Logo' ></img>
       </Link>
 
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarToggler" aria-controls="navBarToggler" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon text-primary"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className='collapse navbar-collapse' id ='navBarToggler'>
         <div className = 'navbar-nav ms-auto'>
-          <NavLink path = '/' text = 'Home'/>
           <NavLink path = '/about' text = 'About Us'/>
           <NavLink path = '/activities' text = 'Activities'/>
           <NavLink path = '/blog' text = 'Blog'/>

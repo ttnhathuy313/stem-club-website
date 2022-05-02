b7f7c826d442b9f644bab550ffd697e38d2038d3
linhvth <vuthihonglinh.work@gmail.com>
2022-05-03 01:38:05 +0700

    update ct

--- website-frontend/src/components/Activities.js
+++ website-frontend/src/components/Activities.js
@@ -1,27 +1,25 @@
 import React from "react";
 import Divider from "./utils/Divider";
 
-import dummy1 from '../images/dummy1.png';
-import dummy2 from '../images/dummy2.png';
-import dummypj1 from '../images/dummypj1.png';
-import dummypj2 from '../images/dummypj2.png';
-import dummypj3 from '../images/dummypj3.png';
-import dummypj4 from '../images/dummypj4.png';
-import dummycompetition1 from '../images/dummycompetition1.png';
-import dummycompetition2 from '../images/dummycompetition2.png';
+import dummy from '../images/Background.png'
 
+const SectionHeader = ({text}) => (
+  <div className='bg-primary text-white text-center rounded pb-1 pt-2 mt-5'>
+    <h3 className=""> {text} </h3>
+  </div>
+)
 
 const Card = ({title, image, date}) => {
   return (
     <div>
-      <div className="card">
+      <div className="card border-0">
         <div className="inner">
             <img className="card-img-top rounded" src={image} alt=""/>
         </div> 
         <div className="card-body">
           <div className="row">
-            <h5 className="card-title text-primary font-weight-bold">{title}</h5>
-            <p className="card-text"><small class="text-muted">[{date}]</small></p>
+            <h6 className="card-title text-primary fw-bold">{title}</h6>
+            <p className="card-text text-secondary fw-bold">[{date}]</p>
           </div>
         </div>
       </div>
@@ -29,73 +27,83 @@ const Card = ({title, image, date}) => {
   )
 }
 
+const SeeMore = ({link}) => {
+  return (
+    <div className="text-end fst-italic text-primary mb-5">
+      <a href={link}>See more</a>
+    </div>
+  )
+}
 
 const Activities = () => {
   return (
     <div className="">
       {/* PROJECT */}
-      <h3 className="text-primary">Project</h3>
-      <Divider />
       <div class="container">
-        <div className="row">
+        <SectionHeader text="Project" />
+        <div className="row mt-5">
           <div className="col-sm">
-            <Card image={ dummypj1 } title='Landscape Research Project' date='Present' />
+            <Card image={ dummy } title='Landscape Research Project' date='Present' />
           </div>
           <div className="col-sm">
-            <Card image={ dummypj2 } title='STEM Club Website' date='Past' />
+            <Card image={ dummy } title='STEM Club Website' date='Past' />
           </div>
           <div className="col-sm">
-            <Card image={ dummypj2 } title='STEM Club Website' date='Past' />
+            <Card image={ dummy } title='STEM Club Website' date='Past' />
           </div>
         </div>
 
-        <div className="row">
+        <div className="row mt-5">
           <div className="col-sm">
-            <Card image={ dummypj3 } title='SQL Workshop Series' date='Upcoming' />
+            <Card image={ dummy } title='SQL Workshop Series' date='Upcoming' />
           </div>
           <div className="col-sm">
-            <Card image={ dummypj4 } title='STEMazine' date='Past'/>
+            <Card image={ dummy } title='STEMazine' date='Past'/>
           </div>
           <div className="col-sm">
-            <Card image={ dummypj2 } title='STEM Club Website' date='Past' />
+            <Card image={ dummy } title='STEM Club Website' date='Past' />
           </div>
         </div>
+
+        <SeeMore link={'https://google.com'}/>
+
       </div>
 
       {/* EVENTS */}
-      <h3 className="text-primary">Events</h3>
-      <Divider />
       <div class="container">
-        <div className="row">
+        <SectionHeader text="Event" />
+        <div className="row mt-5">
           <div class="col-sm">
-            <Card image={ dummy1 } title='Trivia Night' date='Dec 12, 2021' />
+            <Card image={ dummy } title='Trivia Night at Fulbright - an annual playground for STEM enthusiasts' date='Dec 12, 2021' />
           </div>
           <div class="col-sm">
-            <Card image={ dummy2 } title='Sharing with NASA Intern' date='Feb 10, 2022' />
+            <Card image={ dummy } title='Sharing with NASA Intern' date='Feb 10, 2022' />
           </div>
           <div class="col-sm">
-            <Card image={ dummy2 } title='Sharing with NASA Intern' date='Feb 10, 2022' />
+            <Card image={ dummy } title='Sharing with NASA Intern' date='Feb 10, 2022' />
           </div>
         </div>
+
+        <SeeMore link={'https://google.com'}/>
+        
       </div>
 
       {/* COMPETITION */}
-      <h3 className="text-primary">Competition</h3>
-      <Divider />
       <div class="container">
-        <div className="row">
+        <SectionHeader text="Competition" />  
+        <div className="row mt-5">
           <div class="col-sm">
-            <Card image={ dummycompetition1 } title='ICPC 2022' date='Mar 1st, 2022' />
+            <Card image={ dummy } title='ICPC 2022' date='Mar 1st, 2022' />
           </div>
           <div class="col-sm">
-            <Card image={ dummycompetition2 } title='STEM Hackathon' date='Jun 7th, 2021'/>
+            <Card image={ dummy } title='STEM Hackathon' date='Jun 7th, 2021'/>
           </div>
           <div class="col-sm">
-            <Card image={ dummycompetition2 } title='STEM Hackathon' date='Jun 7th, 2021'/>
+            <Card image={ dummy } title='STEM Hackathon' date='Jun 7th, 2021'/>
           </div>
         </div>
+        <SeeMore link={'https://google.com'}/>
       </div>
-
     </div>
   )
 }

155e736af4a5bee79b9e3e2425f2a2394dc022a5
npnkhoi <npnkhoi@gmail.com>
2022-06-04 13:44:38 +0800

    remove weird folder

--- website-frontend/src/components/utils/SectionHeader.js
+++
@@ -1,8 +0,0 @@
-const SectionHeader = ({text}) => (
-  // <div className='bg-primary text-center rounded-pill pb-1 pt-2 mx-5 my-3'>
-  //   <h3 className='text-white'> {text} </h3>
-  // </div>
-  <h2 className='text-center fw-bold mx-auto my-3'> {text} </h2>
-)
-
-export default SectionHeader
